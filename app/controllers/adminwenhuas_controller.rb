1cd9f85aae38c2b156f3e7d7478b06b2a6177329
yunliwei <1127429516@qq.com>
2016-04-13 17:30:55 +0800

    liwei

--- app/controllers/adminwenhuas_controller.rb
+++ app/controllers/adminwenhuas_controller.rb
@@ -47,7 +47,7 @@ class AdminwenhuasController < ApplicationController
         format.json { render :show, status: :ok, location: @adminwenhua }
       else
         format.html { render :edit }
-        format.json { render json: @test.errors, status: :unprocessable_entity }
+        format.json { render json: @adminwenhuas.errors, status: :unprocessable_entity }
       end
     end
   end
--- app/controllers/wenhuas_controller.rb
+++ app/controllers/wenhuas_controller.rb
@@ -1,19 +1,76 @@
 class WenhuasController < ApplicationController
+  before_action :set_adminwenhua, only: [:show, :edit, :update, :destroy]
   def index
 
+    @adminwenhua= Wenhua.all
   end
 
-end
-def create
-  @wenhua = Adminwenhua.new(wenhua_params)
+  # GET /tests/1
+  # GET /tests/1.json
+  def show
 
-  respond_to do |format|
-    if @wenhua.save
-      format.html { redirect_to @wenhua_path, notice: 'Test was successfully created.' }
-      format.json { render :show, status: :created, location: @wenhua }
-    else
-      format.html { render :new }
-      format.json { render json: @wenhua.errors, status: :unprocessable_entity }
+  end
+
+  # GET /tests/new
+  def new
+    @adminwenhua =Wenhua.new
+
+  end
+
+  # GET /tests/1/edit
+  def edit
+
+  end
+
+  # POST /tests
+  # POST /tests.json
+  def create
+    @adminwenhua = Wenhua.new(adminwenhua_params)
+
+    respond_to do |format|
+      if @adminwenhua.save
+        format.html { redirect_to adminwenhuas_path, notice: 'Test was successfully created.' }
+        format.json { render :show, status: :created, location: @adminwenhua }
+      else
+        format.html { render :new }
+        format.json { render json: @adminwenhua.errors, status: :unprocessable_entity }
+      end
     end
   end
+
+  # PATCH/PUT /tests/1
+  # PATCH/PUT /tests/1.json
+  def update
+    respond_to do |format|
+      if @adminwenhua.update(adminwenhuas_paths)
+        format.html { redirect_to adminwenhuas_path, notice: 'Test was successfully updated.' }
+        format.json { render :show, status: :ok, location: @adminwenhua }
+      else
+        format.html { render :edit }
+        format.json { render json: @adminwenhua.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tests/1
+  # DELETE /tests/1.json
+  def destroy
+    @adminwenhua.destroy
+    respond_to do |format|
+      format.html { redirect_to adminwenhuas_path, notice: 'Test was successfully destroyed.' }
+      format.json { head :no_adminwenhua }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_adminwenhua
+    @adminwenhua = Wenhua.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def adminwenhua_params
+    params.require(:wenhua).permit( :title,:content)
+  end
+
 end
