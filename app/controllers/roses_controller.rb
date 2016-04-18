870343a5512796949506da3b33699c222e2ba896
HY <hy888716@gmail.com>
2016-04-18 17:27:08 +0800

    heyao

--- app/controllers/roses_controller.rb
+++ app/controllers/roses_controller.rb
@@ -2,7 +2,16 @@ class RosesController < ApplicationController
   before_action :set_rose, only: [:show, :edit, :update, :destroy]
   def index
 
-    @adminroses = Rose.all
+    @roselefttitle = Rose.all
+
+    if params[:id]
+
+      @content = Rose.find(params[:id])
+
+    else
+      @content = Rose.first
+
+    end
 
   end
 
@@ -14,12 +23,13 @@ class RosesController < ApplicationController
 
   # GET /tests/new
   def new
-    @adminrose = Rose.new
+    @adminrose =Rose.new
 
   end
 
   # GET /tests/1/edit
   def edit
+
   end
 
   # POST /tests
@@ -42,8 +52,8 @@ class RosesController < ApplicationController
   # PATCH/PUT /tests/1.json
   def update
     respond_to do |format|
-      if @adminrose.update(rose_paths)
-        format.html { redirect_to adminroses_path, notice: 'Test was successfully updated.' }
+      if @adminrose.update(rose_params)
+        format.html { redirect_to adminrose_path, notice: 'Test was successfully updated.' }
         format.json { render :show, status: :ok, location: @adminrose }
       else
         format.html { render :edit }
@@ -57,7 +67,7 @@ class RosesController < ApplicationController
   def destroy
     @adminrose.destroy
     respond_to do |format|
-      format.html { redirect_to adminroses_path, notice: 'Test was successfully destroyed.' }
+      format.html { redirect_to adminrose_path, notice: 'Test was successfully destroyed.' }
       format.json { head :no_adminrose }
     end
   end
@@ -65,11 +75,12 @@ class RosesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_rose
-    @rose = Rose.find(params[:id])
+    @adminrose = Rose.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def rose_params
     params.require(:rose).permit( :title,:content)
   end
+
 end
