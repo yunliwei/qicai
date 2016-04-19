46379e567909450938669a885be60699bd32eba7
wcsc575 <1768692079@qq.com>
2016-04-19 17:22:25 +0800

    zw4-19

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,11 @@ Rails.application.routes.draw do
   resources :products
   resources :adminroses
   resources :productclas
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 0aa44c2b735474984120b60cd18df893fff21abc
->>>>>>> d0eff538ae952ef278d5ef0494c7ebc145a280ed
   resources :contacts
   resources :abouts
   resources :adminabouts
-
+  resources :configs
 
 
   namespace :admin do
